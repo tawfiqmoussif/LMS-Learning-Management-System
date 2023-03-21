135178d4dc77962d4d0e12ac773b983457497617
tawfiqmoussif <tawfiqmoussif@gmail.com>
2023-03-21 00:05:21 +0000

    add the most important models and migrations

---
+++ app/Models/DailyAttendance.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DailyAttendance extends Model
+{
+    use HasFactory;
+}
